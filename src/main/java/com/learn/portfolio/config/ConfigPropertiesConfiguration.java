73aec83aa618515621cf78e7a8ad1a5f3f5d3852
Gaurav Rehan <gaurav.rehan@gmail.com>
2021-06-02 11:26:26 +0530

    load properties from config server and config repository

---
+++ src/main/java/com/learn/portfolio/config/ConfigPropertiesConfiguration.java
@@ -0,0 +1,10 @@
+package com.learn.portfolio.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@ConfigurationProperties(prefix="some")
+public class ConfigPropertiesConfiguration {
+
+}
